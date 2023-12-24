545037bb2a97f3ca2cae3aec9ab55044029d830a
unknown <8268363@gmail.com>
2023-12-24 20:52:58 +0300

    update logo; update routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './components/pages/home/home.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
 
 @NgModule({
