f13c8937929be2a24100a30d5f270a6ea2de5fe5
Stanislav Smirnov <stanislavsmwork@gmail.com>
2024-01-22 02:00:46 +0300

    add csurf

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as config from 'config';
 import { ConfigService } from '@nestjs/config';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(csurf());
   const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT') ?? config.get('PORT'));
 }
