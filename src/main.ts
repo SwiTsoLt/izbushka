20898dd4a6a347b69b204a64fc3ef2fa6fe53936
Stanislav Smirnov <stanislavsmwork@gmail.com>
2024-01-22 01:59:55 +0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import config from 'config';
-
-const PORT = process.env.PORT ?? config.get('PORT');
+import * as config from 'config';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(PORT);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('PORT') ?? config.get('PORT'));
 }
 bootstrap();
